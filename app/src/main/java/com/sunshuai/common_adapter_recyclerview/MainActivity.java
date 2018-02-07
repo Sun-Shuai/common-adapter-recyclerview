7ab0cd4ba149f67ca025f1d0a3007c3a7a4d93df
sunshuai <438819781>
2018-02-07 22:18:52 +0800

    整理

--- app/src/main/java/com/sunshuai/common_adapter_recyclerview/MainActivity.java
+++ app/src/main/java/com/sunshuai/common_adapter_recyclerview/MainActivity.java
@@ -2,12 +2,48 @@ package com.sunshuai.common_adapter_recyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.sunshuai.common_adapter_recyclerview_lib.CommonAdapter;
+import com.sunshuai.common_adapter_recyclerview_lib.base.ViewHolder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+
+    private List<String> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        list = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            list.add("===================");
+        }
+
+        initView();
+    }
+
+
+    private void initView() {
+        recyclerView = findViewById(R.id.rv_test);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        recyclerView.setAdapter(new CommonAdapter<String>(this, R.layout.item_recycler_test, list) {
+            @Override
+            protected void convert(ViewHolder holder, String s, int position) {
+                holder.setText(R.id.txt_test, s);
+            }
+        });
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.addItemDecoration(new DividerItemDecoration(
+                this, DividerItemDecoration.HORIZONTAL));
+
     }
 }
